4a077e68511cf0f2d9d183cd5129f9deba294211
ttsoares <ttsoares@orion.ufrgs.br>
2021-11-05 14:43:17 -0300

    Versão 1.0 - tudo testado

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 const port = process.env.PORT || 7777
 
 app.listen(port, () => {
@@ -14,6 +13,13 @@ app.listen(port, () => {
 
 //========================================================
 
+class Transaction {
+		constructor( public id: number, public title:string,  public value: number, public type: string ){}
+	}
+
+class User {
+  constructor(public id:number, public name:string, public cpf:string, public email:string, public age:number, public transaction: Transaction[] = []) {}
+  }
 
 let AllUsers:User[] = [
     {
@@ -77,12 +83,11 @@ let AllUsers:User[] = [
     }
   ]
 
-class User {
-  constructor(public id:number, public name:string, public cpf:string, public email:string, public age:number, public transaction?: Transaction[]) {}
-  }
-  class Transaction {
-    constructor( public id: number, public title:string,  public value: number, public type: string ){}
-  }
+//======= INDICE DA transaction CUJO id É O PASSADO
+function indi_id(userID:number, transID: number): number {
+	const indice = AllUsers[userID].transaction.findIndex(elm => elm.id == transID)
+	return indice
+}
 
 //======= id EXISTE ?
 function idExist(id:number): boolean {
@@ -91,8 +96,8 @@ function idExist(id:number): boolean {
 }
 
 //====== TRANSAÇÃO EXISTE ?
-function transExist(Uid:number, Tid: number): boolean {
-  if (AllUsers[Uid].transaction!.findIndex(elm => elm.id == Tid) >= 0)  return true
+function transExist(userID:number, transID: number): boolean {
+  if (AllUsers[userID].transaction.findIndex(elm => elm.id == transID) >= 0)  return true
   else return false
 }
 
@@ -107,13 +112,14 @@ let Ident: number = AllUsers[AllUsers.length-1].id | 0
 let Ttrans:number = 0
 
 AllUsers.forEach(elm => {
-    Ttrans = Ttrans + (elm.transaction!.length)
+    Ttrans = Ttrans + (elm.transaction.length)
 })
 let trans: number = Ttrans - 1
 //**********************************************************
 
+//******************************* POSTs
 //======ADICIONA USUARIO
-app.post('/users/', (req: Request, res: Response) => {
+app.post('/users', (req: Request, res: Response) => {
 
   const { name, cpf, email, age }: { name: string; cpf: string; email: string; age: number } = req.body;
 
@@ -136,21 +142,19 @@ app.post('/users/', (req: Request, res: Response) => {
 
   if (!unicpf(cpf)) {
     Ident++
-    const Reg:User = new User(Ident, name, cpf, email, age)
+		let tempTrans:Transaction[] = []
+    const Reg:User = new User(Ident, name, cpf, email, age, tempTrans)
     AllUsers.push(Reg)
-    return res.status(201).json(AllUsers)
+    return res.status(201).json(AllUsers[Ident])
   }
   else return res.status(400).send(`<h1>CPF ${cpf} já castrado</h1>`)
-
 })
 
 //======ADICONA TRANSAÇÕES
-app.post('/user/:userid/transactions/', (req: Request, res: Response) => {
-
+app.post('/user/:userid/transactions', (req: Request, res: Response) => {
   const userID:number = Number(req.params.userid)
   const { title, value, type }: { title: string; value: number; type: string } = req.body;
   const Nvalue = Number(value)
-
   if (title.length == 0) {
     return res.status(400).send(`<h1>Campo title vazio</h1>`)
     res.end()
@@ -163,129 +167,61 @@ app.post('/user/:userid/transactions/', (req: Request, res: Response) => {
     return res.status(400).send(`<h1>Transção inválida</h1>`)
     res.end()
   }
-
 	if (!idExist(userID)) {
 		return res.status(400).send(`<h1>Usuário ${userID} não existe</h1>`)
     res.end()
 	}
-
   trans++
   const RegTrans:Transaction = new Transaction(trans, title, Nvalue, type)
-
-  AllUsers[userID].transaction!.push(RegTrans)
-
+  AllUsers[userID].transaction.push(RegTrans)
   res.status(200).send(`<h1>Transação para usuario ${AllUsers[userID].name} cadastrada</h1>`)
 })
 
+//******************************* GETs
 //=======MOSTRA UM USUARIO SEM AS TRANSAÇÕES
 app.get('/users/:id', (req: Request, res: Response) => {
   const userID:number = Number(req.params.id)
   const Nuser = {...AllUsers[userID]}
   delete Nuser.transaction
+	AllUsers[userID].transaction.forEach(elm => console.log(`TransID: ${elm.id}`))
   return res.status(201).json(Nuser)
 })
 
 //========MOSTRA TODOS OS USUARIOS SEM AS TRANSAÇÕES
-app.get('/users/', (req: Request, res: Response) => {
-  const Newarr = [...AllUsers ]
+app.get('/users', (req: Request, res: Response) => {
+	const Newarr:User[] = []
+	AllUsers.forEach(val => Newarr.push((Object.assign({}, val))))
   Newarr.forEach(elm => delete elm.transaction)
-  return res.json(Newarr)
-})
-
-//=======APAGA UM USUARIO
-app.delete('/user/:id', (req: Request, res: Response) => {
-  const userID:number = Number(req.params.id)
-  if(idExist(userID)){
-    AllUsers.splice(userID,1)
-  } else return res.status(400).send(`<h1>Usuario não existe</h1>`)
-})
-
-//========EDITA UM USUARIO
-app.put('/user/:id', (req: Request, res: Response) => {
-  const uID:number = Number(req.params.id)
-
-  if (!idExist(uID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
-
-  const { name, cpf, email, age }: { name: string; cpf: string; email: string; age: number } = req.body;
-
-  if (name.length != 0 && name != AllUsers[uID].name) {
-    AllUsers[uID].name = name
-  }
-  if (cpf.length != 0 && cpf != AllUsers[uID].cpf) {
-    AllUsers[uID].cpf = cpf
-  }
-  if (email.length != 0 && email != AllUsers[uID].email) {
-    AllUsers[uID].email = email
-  }
-  if (!isNaN(age) && age != AllUsers[uID].age) {
-    AllUsers[uID].age = age
-  }
-  return  res.status(200).json(`${AllUsers[uID]}`)
+	return res.json(Newarr)
 })
 
 //=======MOSTRA UMA TRANSAÇÃO DE UM USUÁRIO
-app.post('/user/:userid/transaction/:trasid', (req: Request, res: Response) => {
+app.get('/user/:userid/transaction/:transid', (req: Request, res: Response) => {
 
-  const uID:number = Number(req.params.userid)
-  const tID:number = Number(req.params.transid)
+  const userID:number = Number(req.params.userid)
+  const transID:number = Number(req.params.transid)
 
-  if (!idExist(uID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
-  if (!transExist(uID,tID)) return res.status(400).send(`<h1>Transação não existe</h1>`)
+  if (!idExist(userID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
+  if (!transExist(userID, transID)) return res.status(400).send(`<h1>Transação não existe</h1>`)
 
-  return  res.status(200).json(`${AllUsers[uID].transaction![tID]}`)
-})
-
-//========EDITA UMA TRANSAÇÃO
-app.put('/user/:userid/transaction/:trasid', (req: Request, res: Response) => {
-
-  const uID:number = Number(req.params.userid)
-  const tID:number = Number(req.params.transid)
-
-  if (!idExist(uID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
-  if (!transExist(uID,tID)) return res.status(400).send(`<h1>Transação não existe</h1>`)
-
-  const { title, value, type }: { title: string; value: number; type: string } = req.body;
-
-  if (title.length != 0 && title != AllUsers[uID].transaction![tID].title) {
-    AllUsers[uID].transaction![tID].title = title
-  }
-  if (!isNaN(value) && value != AllUsers[uID].transaction![tID].value) {
-    AllUsers[uID].transaction![tID].value = value
-  }
-  if (type.length != 0 && type != AllUsers[uID].transaction![tID].type) {
-    AllUsers[uID].transaction![tID].type = type
-  }
-
-  return  res.status(200).json(`${AllUsers[uID].transaction![tID]}`)
-})
-
-//=======APAGA UMA TRANSAÇÃO
-app.delete('/user/:userid/transaction/:trasid', (req: Request, res: Response) => {
-
-  const uID:number = Number(req.params.userid)
-  const tID:number = Number(req.params.transid)
-
-  if (!idExist(uID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
-  if (!transExist(uID,tID)) return res.status(400).send(`<h1>Transação não existe</h1>`)
-
-  AllUsers[uID].transaction!.splice(tID,1)
-
-  return  res.status(200).json(`${AllUsers[uID].transaction}`)
+	const indice = indi_id(userID,transID)
+	const saida = AllUsers[userID].transaction[indice]
+  return  res.status(200).json(saida)
 })
 
 //========CALCULA O SALDO DO USUARIO
 app.get('/user/:id', (req: Request, res: Response) => {
 
-  const uID:number = Number(req.params.id)
+	const userID:number = Number(req.params.id)
 
-  if (!idExist(uID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
-  if (typeof AllUsers[uID].transaction !== 'undefined' && AllUsers[uID].transaction!.length === 0)
-    return res.status(400).send(`<h1>Usuario não tem transações</h1>`)
+	if (!idExist(userID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
+	if (typeof AllUsers[userID].transaction !== 'undefined' && AllUsers[userID].transaction.length === 0)
+	return res.status(400).send(`<h1>Usuario não tem transações</h1>`)
 
-  let Positivo:number = 0
-  let Negativo:number = 0
+	let Positivo:number = 0
+	let Negativo:number = 0
 
-	AllUsers[uID].transaction!.forEach(trans => {
+	AllUsers[userID].transaction.forEach(trans => {
 		if (trans.type == "income") {
 			Positivo = Positivo + trans.value
 			console.log(`Depósito: ${trans.value}`)
@@ -296,7 +232,85 @@ app.get('/user/:id', (req: Request, res: Response) => {
 		}
 	})
 
-  const Saldo = Positivo - Negativo
-  console.log(`Saldo: ${Saldo}`)
-  return  res.status(200).json(`Saldo total: ${Saldo}`)
+	const Saldo = Positivo - Negativo
+	console.log(`Saldo: ${Saldo}`)
+	return  res.status(200).json(`Saldo total: ${Saldo}`)
+})
+
+//******************************** PUTs
+//========EDITA UM USUARIO
+app.put('/user/:id', (req: Request, res: Response) => {
+	const userID:number = Number(req.params.id)
+
+	if (!idExist(userID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
+
+	const { name, cpf, email, age }: { name: string; cpf: string; email: string; age: number } = req.body;
+	// CPF é imutável
+
+	if (name.length != 0 && name != AllUsers[userID].name) {
+		AllUsers[userID].name = name
+	}
+	if (email.length != 0 && email != AllUsers[userID].email) {
+		AllUsers[userID].email = email
+	}
+	if (!isNaN(age) && age != AllUsers[userID].age) {
+		AllUsers[userID].age = age
+	}
+	const saida = AllUsers[userID]
+	return  res.status(200).json(saida)
+})
+
+//========EDITA UMA TRANSAÇÃO
+app.put('/user/:userid/transaction/:transid', (req: Request, res: Response) => {
+
+  const userID:number = Number(req.params.userid)
+  const transID:number = Number(req.params.transid)
+
+  if (!idExist(userID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
+  if (!transExist(userID,transID)) return res.status(400).send(`<h1>Transação não existe</h1>`)
+
+  const { title, value, type }: { title: string; value: number; type: string } = req.body;
+
+	const indice = indi_id(userID,transID)
+
+  if (title.length != 0 && title != AllUsers[userID].transaction[indice].title) {
+    AllUsers[userID].transaction[indice].title = title
+  }
+  if (!isNaN(value) && value != AllUsers[userID].transaction[indice].value) {
+    AllUsers[userID].transaction[indice].value = value
+  }
+  if (type.length != 0 && type != AllUsers[userID].transaction[indice].type) {
+    AllUsers[userID].transaction[indice].type = type
+  }
+
+	const saida = AllUsers[userID].transaction[indice]
+
+  return  res.status(200).json(saida)
+})
+
+//******************************** DELETEs
+//=======APAGA UM USUARIO
+app.delete('/user/:id', (req: Request, res: Response) => {
+	const userID:number = Number(req.params.id)
+
+	if(idExist(userID)){
+		const saida = AllUsers[userID]
+		AllUsers.splice(userID,1)
+		return  res.status(200).json(saida)
+	} else return res.status(400).send(`<h1>Usuario não existe</h1>`)
+})
+
+//=======APAGA UMA TRANSAÇÃO
+app.delete('/user/:userid/transaction/:transid', (req: Request, res: Response) => {
+
+  const userID:number = Number(req.params.userid)
+  const transID:number = Number(req.params.transid)
+
+  if (!idExist(userID)) return res.status(400).send(`<h1>Usuario não existe</h1>`)
+  if (!transExist(userID,transID)) return res.status(400).send(`<h1>Transação não existe</h1>`)
+
+	const indice = indi_id(userID,transID)
+  const removida = AllUsers[userID].transaction.splice(indice,1)
+
+  return  res.status(200).json(removida)
 })
