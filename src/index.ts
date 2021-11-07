2ce804634526e7b3aef48e73f0ce1266afbe6314
ttsoares <ttsoares@orion.ufrgs.br>
2021-11-07 08:34:06 -0300

    app.get('/index.html

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'))
 
-app.get('/', function(req, res){
+app.get('/index.html', function(req, res){
     res.sendFile('index.html', { root: __dirname + "/public" } );
 });
 
